35ab5f814435405f147dfd4243152c712cc028f5
mdx97 <mathewhorner456@gmail.com>
2021-06-13 16:46:50 -0500

    Add borders and make them work with window resizes.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use bevy::app::Events;
 use bevy::prelude::*;
 use bevy::sprite::collide_aabb::{collide, Collision};
+use bevy::window::WindowResized;
 
 /// The constant speed the ball moves at.
 const BALL_SPEED: f32 = 250.0;
@@ -27,6 +29,13 @@ const EPSILON: f32 = 0.005;
 /// Constant factor to calculate paddle speed.
 const PADDLE_SPEED: f32 = 500.0;
 
+/// Thickness of the walls that surround the playing area, in pixels.
+const WALL_THICKNESS: f32 = 20.0;
+
+// Default window dimensions.
+const WINDOW_HEIGHT: f32 = 720.0;
+const WINDOW_WIDTH: f32 = 1280.0;
+
 struct Ball {
     velocity: Vec2,
 }
@@ -34,11 +43,14 @@ struct Ball {
 struct Boost;
 struct BoostBackground;
 struct BoostTimer(Timer);
+struct Collider;
 struct Paddle;
 
 struct State {
     boost: f32,
 }
+
+struct Wall(u32);
 
 impl Default for State {
     fn default() -> Self {
@@ -50,6 +62,12 @@ impl Default for State {
 
 fn main() {
     App::build()
+        .insert_resource(WindowDescriptor {
+            title: String::from("Breakout"),
+            height: WINDOW_HEIGHT,
+            width: WINDOW_WIDTH,
+            ..Default::default()
+        })
         .insert_resource(ClearColor(Color::rgb(0.1, 0.1, 0.1)))
         .insert_resource(State::default())
         .insert_resource(BoostTimer(Timer::from_seconds(BOOST_RECHARGE_INTERVAL, true)))
@@ -60,6 +78,7 @@ fn main() {
         .add_system(ball_collision.system())
         .add_system(boost_display.system())
         .add_system(boost_recharge.system())
+        .add_system(window_resize.system())
         .run();
 }
 
@@ -67,8 +86,42 @@ fn startup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>)
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
     commands.spawn_bundle(UiCameraBundle::default());
 
+    let black = materials.add(Color::BLACK.into());
     let white = materials.add(Color::WHITE.into());
 
+    // Create walls
+    commands
+        .spawn_bundle(SpriteBundle {
+            material: black.clone(),
+            transform: Transform::from_xyz(-(WINDOW_WIDTH / 2.0) - (WALL_THICKNESS / 2.0), 0.0, 0.0),
+            sprite: Sprite::new(Vec2::new(WALL_THICKNESS, WINDOW_HEIGHT)),
+            ..Default::default()
+        })
+        .insert(Collider)
+        .insert(Wall(0));
+
+    commands
+        .spawn_bundle(SpriteBundle {
+            material: black.clone(),
+            transform: Transform::from_xyz((WINDOW_WIDTH / 2.0) + (WALL_THICKNESS / 2.0), 0.0, 0.0),
+            sprite: Sprite::new(Vec2::new(WALL_THICKNESS, WINDOW_HEIGHT)),
+            ..Default::default()
+        })
+        .insert(Collider)
+        .insert(Wall(1));
+
+    commands
+        .spawn_bundle(SpriteBundle {
+            material: black.clone(),
+            transform: Transform::from_xyz(0.0, (WINDOW_HEIGHT / 2.0) + (WALL_THICKNESS / 2.0), 0.0),
+            sprite: Sprite::new(Vec2::new(WINDOW_WIDTH, WALL_THICKNESS)),
+            ..Default::default()
+        })
+        .insert(Collider)
+        .insert(Wall(2));
+
+
+    // Create paddle
     commands
         .spawn_bundle(SpriteBundle {
             material: white.clone(),
@@ -76,17 +129,21 @@ fn startup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>)
             sprite: Sprite::new(Vec2::new(200.0, 50.0)),
             ..Default::default()
         })
+        .insert(Collider)
         .insert(Paddle);
 
+
+    // Create ball
     commands
         .spawn_bundle(SpriteBundle {
             material: white.clone(),
-            transform: Transform::from_xyz(0.0, 0.0, 0.0),
+            transform: Transform::from_xyz(-150.0, 0.0, 0.0),
             sprite: Sprite::new(Vec2::new(30.0, 30.0)),
             ..Default::default()
         })
-        .insert(Ball { velocity: Vec2::new(0.0, -BALL_SPEED)});
+        .insert(Ball { velocity: Vec2::new(BALL_SPEED, -BALL_SPEED)});
 
+    // Create Boost HUD
     commands
         .spawn_bundle(SpriteBundle {
             material: white.clone(),
@@ -150,13 +207,13 @@ fn ball_collision(
     mut query: QuerySet<(
         Query<&mut Ball>,
         Query<(&Ball, &Sprite, &Transform)>,
-        Query<(&Sprite, &Transform)>,
+        Query<(&Sprite, &Collider, &Transform)>,
     )>
 ) {
     let mut velocity = query.q0_mut().single_mut().unwrap().velocity.clone();
 
     if let Ok((_, sprite, transform)) = query.q1().single() {
-        for (other_sprite, other_transform) in query.q2().iter() {
+        for (other_sprite, _, other_transform) in query.q2().iter() {
             if let Some(collision) = collide(other_transform.translation.clone(), other_sprite.size.clone(), transform.translation.clone(), sprite.size.clone()) {
                 match collision {
                     Collision::Left | Collision::Right => { velocity.x = -velocity.x },
@@ -189,5 +246,30 @@ fn boost_recharge(
     if timer.0.tick(time.delta()).finished() {
         state.boost += BOOST_RECHARGE;
         state.boost = state.boost.clamp(0.0, 100.0)
+    }
+}
+
+/// System for handling the position / size of elements when the screen resizes.
+fn window_resize(event: Res<Events<WindowResized>>, mut query: Query<(&Wall, &mut Sprite, &mut Transform)>) {
+    // TODO: Need to handle the location of the HUD.
+    // TODO: Need to handle paddle limits.
+    for e in event.get_reader().iter(&event) {
+        for (wall, mut sprite, mut transform) in query.iter_mut() {
+            match wall.0 {
+                0 => {
+                    *sprite = Sprite::new(Vec2::new(WALL_THICKNESS, e.height));
+                    transform.translation.x = -(e.width / 2.0) - (WALL_THICKNESS / 2.0);
+                },
+                1 => {
+                    *sprite = Sprite::new(Vec2::new(WALL_THICKNESS, e.height));
+                    transform.translation.x = (e.width / 2.0) + (WALL_THICKNESS / 2.0);
+                },
+                2 => {
+                    *sprite = Sprite::new(Vec2::new(e.width, WALL_THICKNESS));
+                    transform.translation.y = (e.height / 2.0) + (WALL_THICKNESS / 2.0);
+                },
+                _ => { },
+            }
+        }
     }
 }
